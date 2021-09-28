4fc56b7d13f3e01de569b25003376f8eb1713f6e
Abdir3 <70133379+Abdir3@users.noreply.github.com>
2021-09-28 21:13:15 +0200

    First draft game works
    added logic to remove pictures when user guesses wrong

--- Arbeidskrav/hovedtask.js
+++ Arbeidskrav/hovedtask.js
@@ -1,10 +1,15 @@
-//Henter html elemeneter
+//Henter alle html elementer som jeg kommer til å bruke i funksjonene
 let button = document.getElementById('btn');
 let button1 = document.getElementById('btn1');
 const bildeLiv = document.getElementById('liv'); // Der 
 let riktig_poeng = document.getElementById('riktig');
 let feil_poeng = document.getElementById('feil');
 let win = document.getElementById('win');
+let img1 = document.getElementById('1');
+let img2 = document.getElementById('2');
+let img3 = document.getElementById('3');
+let img4 = document.getElementById('4');
+let img5 = document.getElementById('5');
 
 
 // funksjon for å  randomiserer hvilken knapp som er riktig 
@@ -19,6 +24,7 @@ function checker () {
         feil_poeng.innerHTML = `Antall Feil: ${antall_Feil}`;
         checkLoser();
         remove();
+       
     }
 }   
 // event som fører til at knappene fører til at funksjonen checker utføres
@@ -27,39 +33,43 @@ button1.onclick = checker;
 // sjekker vinner 
 function checkWinner () {
     if (antall_Poeng == 10) {
+    bildeLiv.remove();
+    feil_poeng.remove();
+    riktig_poeng.remove();
+    feil_poeng.remove ();
     win.innerHTML = `<img src = "/images/fireworks.jpg">`; 
     } 
 }
-// sjekker om
+// Funksjon som fjerner liv visuelt og når spiller har fått 5 feil avslutter den spillet 
 function checkLoser(){
-    if(antall_Feil == 5){
-    win.innerHTML = `<img src = "/images/gameover.jpg">`;  
+    if(antall_Feil == 1) {
+        img1.remove();
+    } else if (antall_Feil ==2) {
+        img2.remove();
     
+    } else if (antall_Feil ==3) {
+        img3.remove();
+    
+    } else if (antall_Feil ==4) {
+       img4.remove();
+    }
+    else if(antall_Feil == 5){
+    win.innerHTML = `<img src = "/images/cat.jpg">`;  
+    bildeLiv.remove();
+    feil_poeng.remove();
+    riktig_poeng.remove();
+    feil_poeng.remove ();
+    win.remove
     }
 }
 
-// kontroll variabeler 
+// kontroll variabeler for funksjonene ovenfor for å kontrollere spillet
 let liv_Igjen = 5;
 let  antall_Poeng = 0;
 let antall_Feil = 0;
 
 
-//   Lager en funksjon som lager 5 bilder
-function liv () {
-    for( let i = 0;  i <= 5;  i++) { 
-   bildeLiv.innerHTML += `<img src = "/images/person3.jpg">`; 
-    }    
-}
-//Kalle funksjonen for å produsere 5 bilder
- liv ();
 
- //Funksjon for å fjerne bilder does not currently work...
-
- function remove () {
-    const deletePictures = document.getElementById('liv');
-    deletePictures.parentNode.removeChild(deletePictures.ChildNodes[0]);
-    
- }
 
 
 
