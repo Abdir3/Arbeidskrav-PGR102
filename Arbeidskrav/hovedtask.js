ac4a215f165002b32a40998731c6070b4a73aa1d
Abdir3 <70133379+Abdir3@users.noreply.github.com>
2021-09-28 21:46:24 +0200

    bedret funksjonene

--- Arbeidskrav/hovedtask.js
+++ Arbeidskrav/hovedtask.js
@@ -4,7 +4,8 @@ let button1 = document.getElementById('btn1');
 const bildeLiv = document.getElementById('liv'); 
 let riktig_poeng = document.getElementById('riktig');
 let feil_poeng = document.getElementById('feil');
-const body = document.getElementById('win')
+let text = document.getElementById('text');
+const body = document.getElementById('win');
 let win = document.getElementById('win');
 let img1 = document.getElementById('1');
 let img2 = document.getElementById('2');
@@ -13,14 +14,14 @@ let img4 = document.getElementById('4');
 let img5 = document.getElementById('5');
 
 
-// kontroll variabeler for funksjonene ovenfor for å kontrollere spillet
+// kontroll variabeler for funksjonene 
 let liv_Igjen = 5;
 let  antall_Poeng = 0;
 let antall_Feil = 0;
 
 
 
-// hovedfunksjon, randomiserer knappene og kjører de andre funksjonene
+// Hovedfunksjon, randomiserer knappene, og kjører de andre funksjonene
 function checker () {
     let random = Math.floor(Math.random()*2+1);
     if (random === 1 ) {
@@ -35,17 +36,23 @@ function checker () {
        
     }
 }   
-// event knapper som kjører hovedfunksjonen
+// Event knapper som kjører hovedfunksjonen
 button.onclick = checker;  
 button1.onclick = checker;
-// funksjon som sjekker vinner  og avslutter spillet hvis spilleren har vunnet 
+// funksjon som sjekker vinner  og avslutter hvis spilleren har vunnet ved å gjette riktig 10 ganger
 function checkWinner () {
     if (antall_Poeng == 10) {
-   body.remove();
-    win.innerHTML = `<img src = "/images/fireworks.jpg">`; 
+        bildeLiv.remove();
+        feil_poeng.remove();
+        riktig_poeng.remove();
+        feil_poeng.remove ();
+        button.remove();
+        button1.remove();
+        text.remove();
+      win.innerHTML = `<img src = "/images/fireworks.jpg">`; 
     } 
 }
-// Funksjon som fjerner liv visuelt og når spiller har fått 5 feil avslutter den spillet 
+// Funksjon som fjerner liv visuelt og avslutter spillet når spilleren har gjettet feil 5 ganger
 function checkLoser(){
     if(antall_Feil == 1) {
         img1.remove();
@@ -63,6 +70,9 @@ function checkLoser(){
     feil_poeng.remove();
     riktig_poeng.remove();
     feil_poeng.remove ();
+    button.remove();
+    button1.remove();
+    text.remove();
     }
 }
 
