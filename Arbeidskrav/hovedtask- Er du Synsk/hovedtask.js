76ab2fb35c80f3e5a22bbb5ef0ec365802d24bff
Abdir3 <70133379+Abdir3@users.noreply.github.com>
2021-10-10 13:58:43 +0200

    Update hovedtask.js

--- Arbeidskrav/hovedtask- Er du Synsk/hovedtask.js
+++ Arbeidskrav/hovedtask- Er du Synsk/hovedtask.js
@@ -6,7 +6,7 @@ let riktig_poeng = document.getElementById('riktig');
 let feil_poeng = document.getElementById('feil');
 let text = document.getElementById('text');
 const body = document.getElementById('win');
- let input = prompt('Hvor mange liv vil du ha?');
+ let input = prompt('Vel antall forsøk du ønsker', 'Forsøk = Liv //Skriv kun tall');
 
 
  // kontroll variabeler for funksjonene 
@@ -19,13 +19,13 @@ button.onclick = checker;
 button1.onclick = checker;
 
 //Lager et objekt for tallene 3,5 og 7 og setter disse til true, slik at jeg kan bruke dem senere
-let liv = 
+/*let liv = 
     {
         3: true,
         5: true,
         7: true
     }
- 
+ */
 
 // Lager en for-loop som lager antall liv  visuelt dynamisk basert på bruker-input
 for(let i = 0; i < input; i++){
@@ -40,6 +40,11 @@ function checker () {
         antall_Poeng ++; 
         riktig_poeng.innerHTML = `Antall Poeng: ${antall_Poeng}`;
         checkWinner ();
+    }
+        if(antall_Poeng === 3 || antall_Poeng === 5 || antall_Poeng === 7 ){
+            bildeLiv.innerHTML += `<img class = 'life' src="/images/person3.png" alt="">`
+            liv_Igjen++   
+            checkWinner ();
     } else {
         antall_Feil ++;
         liv_Igjen --; 
@@ -49,17 +54,14 @@ function checker () {
         checkLoser()
     }
 
-    if(antall_Poeng === 3 || antall_Poeng === 5 || antall_Poeng === 7 ){
-         /* Hvis tallet er 3,5 eller 7 vil den sjekke objektet "antall liv" og hvis den er true legger den til 
-          et bilde og setter den verdien til false. Dette fordi  hvis du har 3 riktig,
-           og du gjetter feil er verdien forsatt 3 riktige, i denne scenarion vil den lagt til et bilde men siden vi 
-           gjorde 3 til false vil den ikke legge til et bilde hvis du skulle gjette feil når du har enten 3, 5 eller 7 poeng. */ 
+    /*if(antall_Poeng === 3 || antall_Poeng === 5 || antall_Poeng === 7 ){
+        
         if(liv[antall_Poeng])  {
             bildeLiv.innerHTML += `<img class = 'life' src="/images/person3.png" alt="">`
         liv[antall_Poeng] = false
         liv_Igjen++
         }
-    }
+    }*/ 
 }   
 
 
